5de5bcaa657d84d0cb49106c74825694424b522e
GAFievet <guy.aurele@gmail.com>
2025-02-10 17:02:00 +0100

    bv_freq_retreived

--- bv_frequencies.py
+++ bv_frequencies.py
@@ -12,32 +12,32 @@ from matplotlib.text import Text
 p = os.path.join(r'C:\Users\G to the A\Desktop\MT\Programming\CTD',
                  'PROCESSED_data_POS_CORRECTED_above2mREMOVED_ww11.mat')
 data = sio.loadmat(p, squeeze_me = True)
-# Set clear variables
+
 date = data['time'][0:915]
 cond = data['conductivity'][0:915]
-depth = data['depth'][0:915]
-lat = data['latitude'][0:915]
+depth = data['depth']
 lon = data['longitude'][0:915]
+lat = data['latitude'][0:915]
 pressure = data['pressure'][0:915]
 salinity = data['salinity'][0:915]
 temp = data['temperature'][0:915]
-# Convert to python datetime
 date = [(datetime(1, 1, 1) + timedelta(days = matlab_date - 367)) for matlab_date in date]
-# date=np.array(date)
 del data, p
-
 # Calculate buoyancy frequency (N^2)
+n2, p = gsw.Nsquared(salinity.T, temp.T, pressure.T, lat.T)
 n = np.sqrt(np.abs(n2.T))  # Transpose n2 back and calculate N
 n[np.isinf(n)] = np.nan
+# n = n[0:915, :]
 # n[n < 0.025] = np.nan
 
 # Plotting the Hovmoller diagram
 fig, ax = plt.subplots(figsize = (10, 2.5))
+x = date
+y = depth[0:-1]
 Z = n.T.tolist()
 X, Y = np.meshgrid(x, y)
 
 contourf = ax.contourf(X, Y, Z, 30, cmap = 'jet')
-
 
 # To create custom evenly spaced xticks
 # ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%d %H:%M'))
@@ -55,12 +55,19 @@ ax.set_ylabel('Depth (m)')
 ax.set_xlabel('Time')
 cbar = fig.colorbar(contourf, ax = ax, orientation = 'horizontal')
 
+ylim = -100
 ax.set_ylim([ylim, 0])
+date_num = mdates.date2num(date)
 ax.set_xlim([date_num.min(), date_num.max()])
 
 storm_start = mdates.date2num(datetime(2022, 9, 26))
 storm_end = mdates.date2num(datetime(2022, 9, 30))
-                     edgecolor = 'red', lw = 2)
+l = storm_end - storm_start
+rect = plt.Rectangle((storm_start, ylim), l, abs(ylim), facecolor = 'none', edgecolor = 'red', lw = 2)
+text = Text(storm_start + l / 2, ylim + 10 * 100 / abs(ylim), 'Storm', ha = 'center', va = 'center', color = 'red',
+            fontsize = 11, )
+ax.add_patch(rect)
+ax.add_artist(text)
 
 plt.tight_layout()
 plt.show()

36b5838630bfbfe96ea9c3413a7c8a4a545d7746
GAFievet <guy.aurele@gmail.com>
2025-04-23 18:24:30 +0200

    Surface oceanic current translation v4 (plot ok for each day from 23/09/22 to 16/10/22)

--- Oceanic_currents/surface_current_map.py
+++ Oceanic_currents/surface_current_map.py
@@ -4,7 +4,6 @@ import pickle
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
-import numpy as np
 
 
 def plot_oceanic_currents(u, v, lons, lats, date):
@@ -35,38 +34,43 @@ def plot_oceanic_currents(u, v, lons, lats, date):
 	ax.add_feature(cfeature.COASTLINE)
 
 	# 5. Determine the density of the arrows for plotting
-	arrow_density = 10  # Adjust this value to change the density of arrows
-	lon_step = (lons.size // arrow_density) or 1
-	lat_step = (lats.size // arrow_density) or 1
-
-	# 6. Create a meshgrid for the longitude and latitude coordinates, subsampled for arrow density
-	lon_plot, lat_plot = np.meshgrid(lons[::lon_step], lats[::lat_step])
-	u_plot = u[::lat_step, ::lon_step]
-	v_plot = v[::lat_step, ::lon_step]
-
-	# 7. Calculate the magnitude of the current for scaling the arrows
-	magnitude = np.sqrt(u_plot ** 2 + v_plot ** 2)
+	# # target_num_arrows = 50  # Aim for around 50 arrows in the plot (adjust as needed)
+	# # lon_step = max(lons.size // int(np.sqrt(target_num_arrows)), 1)
+	# # lat_step = max(lats.size // int(np.sqrt(target_num_arrows)), 1)
+	# arrow_density = 10  # Adjust this value to change the density of arrows
+	# lon_step = (lons.size // arrow_density) or 1
+	# lat_step = (lats.size // arrow_density) or 1
+	#
+	# # 6. Create a meshgrid for the longitude and latitude coordinates, subsampled for arrow density
+	# lon_plot, lat_plot = np.meshgrid(lons[::lon_step], lats[::lat_step])
+	# u_plot = u[::lat_step, ::lon_step]
+	# v_plot = v[::lat_step, ::lon_step]
+	#
+	# # 7. Calculate the magnitude of the current for scaling the arrows
+	# magnitude = np.sqrt(u_plot ** 2 + v_plot ** 2)
 
 	# 8. Add the vector field plot (quiver) with scaled arrows
-	Q = ax.quiver(lon_plot, lat_plot, u_plot, v_plot,
-	              scale = 20,  # Adjust this scale factor for arrow length.  Changed from 50 to 20.
-	              regrid_shape = 15,  # helps with evenly spaced arrows
+	Q = ax.quiver(lons, lats, u, v,
+	              scale = 2 / 2.54,  # Adjust this scale factor for arrow length.  The reference unit is 0.5cm.
+	              regrid_shape = 20,  # helps with evenly spaced arrows
+	              scale_units = 'inches'
 	              )
 
 	# 9. Add a scale arrow
-	scale_value = 1  # m/s
-	x_scale = lonlim[0] + 0.2
+	scale_value = 0.5  # m/s
+	x_scale = lonlim[1] - 0.4
 	y_scale = latlim[0] + 0.2
 	ax.quiverkey(Q, x_scale, y_scale, scale_value, f'{scale_value} m/s',
-	             labelpos = 'E', coordinates = 'data', color = 'k',
+	             labelpos = 'N', coordinates = 'data', color = 'k',
 	             fontproperties = {'size': 10})
 
 	# 10. Add title with the date
-	plt.title(f'Surface Oceanic Current Velocities on {date.strftime("%Y-%m-%d")}, (72h low-passed and daily '
+	plt.title(f'Surface Oceanic Current Velocities on {date.strftime("%d/%m/%Y")}, (72h low-passed and daily '
 	          f'averaged)')
 
-	# 11. Display the plot
-	plt.show()
+
+# 11. Display the plot
+plt.show()
 
 
 if __name__ == '__main__':
@@ -100,12 +104,21 @@ if __name__ == '__main__':
 		with (open(path2file, 'rb') as f):
 			data2plot = pickle.load(f)
 			print(f"The file '{path2file}' was imported successfully.")
-			u_all, v_all, lons, lats, t = data2plot['u_d_avg'], data2plot['v_d_avg'], data2plot['lon_ibi'], data2plot[
-				'lat_ibi'], data2plot['t2']
-			u, v = u_all[:, :, 0], v_all[:, :, 0]
+			u = data2plot['u_d_avg']
+			v = data2plot['v_d_avg']
+			lons = data2plot['lon_ibi']
+			lats = data2plot['lat_ibi']
+			t = data2plot['t2']
 
 			# Plot
-			plot_oceanic_currents(u, v, lats, lons, t[0])
+			for d in range(len(t)):
+				print(d)
+				plot_oceanic_currents(u[:, :, d], v[:, :, d], lons, lats, t[d])
+				# Save fig
+				plt.savefig(f'../plots/surface_oceanic_currents/ {t[d].strftime("%Y%m%d")}.png', transparent =
+				False, bbox_inches = 'tight')
+				plt.close()
+
 	except FileNotFoundError:
 		print(f"Error: The file '{path2file}' was not found.")
 	except Exception as e:
